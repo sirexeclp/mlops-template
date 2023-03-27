8e54807f1e6997156e7f403a4d12096cd631f8a4
Felix Grzelka <felix.grzelka@hpi.de>
2023-03-27 18:52:48 +0200

    add output to install script

--- hooks/post_gen_project.sh
+++ hooks/post_gen_project.sh
@@ -2,13 +2,18 @@
 
 git init
 
+echo "Creating a new python venv in $PWD/venv"
 python3 -m venv venv
 source ./venv/bin/activate
 echo "Installing the following python dependencies using pip"
 cat requirements.txt
 pip3 install -q -r requirements.txt
 
+echo "Installing git pre-commit hooks"
 pre-commit install
 
+echo "Creating initial git commit"
 git add .
 git commit -m "init"
+
+echo "DONE"
